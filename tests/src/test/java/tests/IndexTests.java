3244f45d078c17ecdce08f954f292d2819300d91
Maciej Fec <maciejfec1996@gmail.com>
2022-06-26 18:41:01 +0100

    added a base test

--- tests/src/test/java/tests/IndexTests.java
+++ tests/src/test/java/tests/IndexTests.java
@@ -1,4 +1,66 @@
 package tests;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class IndexTests {
+
+    WebDriver driver;
+    String baseUrl = "https://fecrol.github.io/ColourFlipper/";
+
+    @BeforeEach
+    public void setUp() {
+
+        String browser = System.getProperty("browser");
+
+        if(browser == null) {
+            browser = "";
+        }
+
+        switch(browser) {
+            case "chrome":
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
+                break;
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+                break;
+            case "edge":
+                WebDriverManager.edgedriver();
+                driver = new EdgeDriver();
+                break;
+            default:
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
+                break;
+        }
+
+        driver.manage().window().maximize();
+        driver.get(baseUrl);
+    }
+
+    @AfterEach
+    public void teardown() {
+
+        driver.quit();
+    }
+
+    @Test
+    @DisplayName("Is able to apply background colour using apply button")
+    public void applyBackgroundWithBtn() {
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
